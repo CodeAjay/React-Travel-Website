8d13300dfbcfbccda2e2f8165b4dc86fea43a6c6
CodeAjay <58427577+CodeAjay@users.noreply.github.com>
2023-01-24 20:35:51 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testin my app
+          Testin my app here is a demo
         </p>
       </header>
     </div>
